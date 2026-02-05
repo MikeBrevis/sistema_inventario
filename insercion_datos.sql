de531f88d852f102ab9fe721f243438f3ff39b8b
MikeBrevis <michel.brevis@gmail.com>
2026-02-05 17:13:22 -0300

    Join entre tablas cliente - vendedor

--- insercion_datos.sql
+++ insercion_datos.sql
@@ -12,5 +12,40 @@ VALUES
 (9, 'Sofía Castro', '988990011', 'scastro@empresa.com', DEFAULT, DEFAULT),
 (10, 'Andrés Vera', '999001122', 'avera@empresa.com', DEFAULT, DEFAULT);
 
+INSERT INTO cliente (
+    id_cliente, nombre, telefono, correo, fecha_de_creacion, fecha_de_actualizacion
+)
+VALUES 
+(11, 'Mateo Salazar', '910203040', 'mateo.salazar@empresa.com', DEFAULT, '2026-02-05'),
+(12, 'Isabella Ortiz', '920304050', 'i.ortiz@empresa.com', DEFAULT, '2026-02-05'),
+(13, 'Alejandro Peña', '930405060', 'ale.pena@empresa.com', DEFAULT, '2026-02-05'),
+(14, 'Camila Blanco', '940506070', 'cblanco@empresa.com', DEFAULT, '2026-02-05'),
+(15, 'Nicolás Herrera', '950607080', 'n.herrera@empresa.com', DEFAULT, '2026-02-05'),
+(16, 'Gabriela Méndez', '960708090', 'gabriela.m@empresa.com', DEFAULT, '2026-02-05'),
+(17, 'Samuel Vargas', '970809010', 'samuel.vargas@empresa.com', DEFAULT, '2026-02-05'),
+(18, 'Martina Ríos', '980901020', 'mrios@empresa.com', DEFAULT, '2026-02-05'),
+(19, 'Joaquín Silva', '990102030', 'j.silva@empresa.com', DEFAULT, '2026-02-05'),
+(20, 'Victoria Lara', '911335577', 'vlara@empresa.com', DEFAULT, '2026-02-05');
+
 select * from empleado
+
+--Selecciona columnas de tablas
+select id_cliente, nombre from cliente;
+
+insert into cotizacion (
+	id_cotizacion, id_cliente_fk, id_empleado_fk, fecha_de_venta, tipo_de_contrato
+)
+
+values (501, 11, 1, '2026-02-05', 'venta directa' )
+
+select * from cotizacion;
+
+
+
+
+
+
+
+
+
 
