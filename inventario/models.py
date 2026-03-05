afe80215488d681bbb0f939dc3852ac45ebb46a2
MikeBrevis <michel.brevis@gmail.com>
2026-03-05 17:35:24 -0300

    Aplicaciones Apps, sucursal e insumo

--- inventario/models.py
+++ inventario/models.py
@@ -1,3 +1,19 @@
 from django.db import models
 
-# Create your models here.
+# Molde para las Sucursales
+class Sucursal(models.Model):
+    nombre_sucursal = models.CharField(max_length=100) # CharField es para textos cortos
+
+    # Esta función le dice a Django cómo mostrar el nombre de la sucursal en texto
+    def __str__(self):
+        return self.nombre_sucursal
+
+
+# Molde para el Catálogo de Insumos
+class Insumo(models.Model):
+    nombre = models.CharField(max_length=150)
+    descripcion = models.TextField(blank=True, null=True) # TextField es para textos largos
+
+    def __str__(self):
+        return self.nombre
+    
