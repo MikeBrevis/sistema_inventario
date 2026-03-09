f2284aadba9136a02986d9906cbd6e0963d4207d
MikeBrevis <michel.brevis@gmail.com>
2026-03-08 21:36:11 -0300

    import class to django admin

--- inventory/admin.py
+++ inventory/admin.py
@@ -1,3 +1,11 @@
 from django.contrib import admin
+from .models import Office, Supplies, Inventory, Request, Request_detail, Delivery_history
 
-# Register your models here.
+admin.site.register(Office)
+admin.site.register(Supplies)
+admin.site.register(Inventory)
+admin.site.register(Request)
+admin.site.register(Request_detail)
+admin.site.register(Delivery_history)
+
+
